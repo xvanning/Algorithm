2085cccb72a577b7a97fd5acc2f5e1f2eff7b98d
xvanning <130809016@qq.com>
2018-11-02 11:33:39 +0800

    2018-11-2

--- 剑指offer/53 表示数值的字符串.java
+++ 剑指offer/53 表示数值的字符串.java
@@ -9,15 +9,15 @@ public class Solution {
         for(int i = 0; i < str.length; i ++){
             //判断加减号
             if(( str[i] == '+' || str[i] == '-')){
-                if(i == 0){
+                if(i == 0){ //字符串开头，继续
                     continue;
-                }else{
-                    if(usedE == false){
+                }else{//如果不是字符串开头
+                    if(usedE == false){ // 如果前面没有出现E 或者 e
                         return false;
                     }else{
-                        if(str[i - 1] == 'e' || str[i - 1] == 'E'){
+                        if(str[i - 1] == 'e' || str[i - 1] == 'E'){ // 如果符号前面是E 或者 e
                             continue;
-                        }else{
+                        }else{ //其他情况
                             return false;
                         }
                     }
@@ -26,10 +26,10 @@ public class Solution {
 
             //判断小数点
             if(str[i] == '.'){
-                if(usedDot == true){
+                if(usedDot == true){ //只能出现一个小数点
                     return false;
                 }
-                if(usedE == true){
+                if(usedE == true){ // 出现在 E 或者 e 后面
                     return false;
                 }
                 usedDot = true;
@@ -38,10 +38,10 @@ public class Solution {
 
             //判断e E
             if((str[i] == 'e' || str[i] == 'E') ){
-                if(usedE == true){
+                if(usedE == true){ //只能出现一个E 或者 e
                     return false;
                 }
-                if(i == str.length - 1){
+                if(i == str.length - 1){ //指数在字符串末尾
                     return false;
                 }
                 usedE = true;
@@ -49,7 +49,7 @@ public class Solution {
             }
 
             //判断数字
-            if(str[i] < '0' || str[i] > '9'){
+            if(str[i] < '0' || str[i] > '9'){ //非数字的情况
                 return false;
             }
 
