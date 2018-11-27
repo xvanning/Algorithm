1cf560ac3f8c7846caeacea92eeed4e49ef0f036
xvanning <130809016@qq.com>
2018-11-27 16:33:12 +0800

    2018-11-27

--- 剑指offer/排序算法/（归并排序）MergeSort.java
+++ 剑指offer/排序算法/（归并排序）MergeSort.java
@@ -37,12 +37,4 @@ class Solution{
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {9,8,7,6,5,4,3,2,1,0};
-        Solution solution = new Solution();
-        solution.Sort(arr);
-        for(int i = 0; i < arr.length; i ++){
-            System.out.print(arr[i] + " ");
-        }
-    }
 }
---
+++ 剑指offer/排序算法/（快速排序）QuickSort.java
@@ -0,0 +1,38 @@
+class Solution{
+    public void Sort(int[] arr){
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+    }
+
+    public void quickSort(int[] arr, int l , int r){
+        if(l >= r){
+            return;
+        }
+        int p = partition(arr, l , r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+
+    public int partition(int[] arr, int l, int r){
+        swap(arr, l, (int) (Math.random() * (r - l + 1)) + l);
+
+        int v = arr[l];
+        int j = l;
+
+        for(int i = l + 1; i <= r; i ++){
+            if(arr[i] < v){
+                j ++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, l, j);
+
+        return j;
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
