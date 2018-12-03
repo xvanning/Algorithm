7fc6a1238a202c3b044400f4febae4ea5469cf42
xvanning <130809016@qq.com>
2018-12-03 17:23:37 +0800

    2018-12-3

--- 剑指offer/动态规划/7 斐波那契数列.java
+++ 剑指offer/动态规划/7 斐波那契数列.java
@@ -4,10 +4,7 @@ public class Solution {
         if(n <= 0){
             return 0;
         }
-        if(n == 1){
-            return 1;
-        }
-        if(n == 2){
+        if(n == 1 || n == 2){
             return 1;
         }
         int[] fib = new int[n + 1];
