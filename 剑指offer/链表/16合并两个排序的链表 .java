f75d87be44646333ab6041fb691f983932fd943e
xvanning <130809016@qq.com>
2019-10-03 10:59:36 +0800

    2019-10-3

--- 剑指offer/链表/16合并两个排序的链表 .java
+++ 剑指offer/链表/16合并两个排序的链表 .java
@@ -31,10 +31,12 @@ public class Solution {
         while(list1 != null){
             dummy.next = list1;
             list1 = list1.next;
+            dummy = dummy.next;
         }
         while(list2 != null){
             dummy.next = list2;
             list2 = list2.next;
+            dummy = dummy.next;
         }
         return retHead.next;
     }
