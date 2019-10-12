a74c736ab37965f5666c88beaef5c70fb9ca8342
xvanning <130809016@qq.com>
2019-10-12 23:35:08 +0800

    修改合并链表

--- 剑指offer/链表/16合并两个排序的链表 .java
+++ 剑指offer/链表/16合并两个排序的链表 .java
@@ -10,35 +10,25 @@ public class ListNode {
 //构建一个虚拟头节点。谁小谁在前面。
 public class Solution {
     public ListNode Merge(ListNode list1,ListNode list2) {
-        if(list1 == null){
-            return list2;
-        }
-        if(list2 == null){
-            return list1;
-        }
         ListNode dummy = new ListNode(-1);
-        ListNode retHead = dummy;
+        ListNode cur = dummy;
         while (list1 != null && list2 != null) {
-            if(list1.val <= list2.val){
-                dummy.next = list1;
+            if (list1.val <= list2.val) {
+                cur.next = list1;
                 list1 = list1.next;
-            }else{
-                dummy.next = list2;
+            } else {
+                cur.next = list2;
                 list2 = list2.next;
             }
-            dummy = dummy.next;
+            cur = cur.next;
         }
-        while(list1 != null){
-            dummy.next = list1;
-            list1 = list1.next;
-            dummy = dummy.next;
+        if (list1 != null){
+            cur.next = list1;
         }
-        while(list2 != null){
-            dummy.next = list2;
-            list2 = list2.next;
-            dummy = dummy.next;
+        if (list2 != null){
+            cur.next = list2;
         }
-        return retHead.next;
+        return dummy.next;
     }
 }
 
